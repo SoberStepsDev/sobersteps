765d0018a7be2bf5ffe153e59c93d68ea28ba8c1
SoberStepsDev <263256049+SoberStepsDev@users.noreply.github.com>
2026-04-01 20:59:08 -0400

    fix: eliminate Java 8 obsolete warnings by forcing Java 17 and Kotlin 1.9

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -26,8 +26,17 @@ gradle.projectsEvaluated {
     subprojects {
         tasks.withType<KotlinCompile>().configureEach {
             compilerOptions {
-                languageVersion.set(KotlinVersion.KOTLIN_1_8)
-                apiVersion.set(KotlinVersion.KOTLIN_1_8)
+                languageVersion.set(KotlinVersion.KOTLIN_1_9)
+                apiVersion.set(KotlinVersion.KOTLIN_1_9)
+                jvmTarget.set(org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_17)
+            }
+        }
+        
+        // Force Java 17 for all subprojects to eliminate "source value 8 is obsolete" warnings
+        plugins.withType<JavaPlugin> {
+            extensions.configure<JavaPluginExtension> {
+                sourceCompatibility = JavaVersion.VERSION_17
+                targetCompatibility = JavaVersion.VERSION_17
             }
         }
     }
