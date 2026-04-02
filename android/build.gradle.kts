4ac2526716ca66291f39430389156661543bb76f
SoberStepsDev <263256049+SoberStepsDev@users.noreply.github.com>
2026-04-01 21:14:42 -0400

    fix: revert global JVM override, set Java 11 only in app module to fix 59 errors in onesignal_flutter

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -1,6 +1,3 @@
-import org.jetbrains.kotlin.gradle.dsl.KotlinVersion
-import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
-
 allprojects {
     repositories {
         google()
@@ -20,25 +17,6 @@ subprojects {
 }
 subprojects {
     project.evaluationDependsOn(":app")
-}
-
-gradle.projectsEvaluated {
-    subprojects {
-        // Fix for "Inconsistent JVM Target Compatibility"
-        // Force both Java and Kotlin tasks to use JVM 17
-        tasks.withType<JavaCompile>().configureEach {
-            sourceCompatibility = "17"
-            targetCompatibility = "17"
-        }
-        
-        tasks.withType<KotlinCompile>().configureEach {
-            compilerOptions {
-                jvmTarget.set(org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_17)
-                languageVersion.set(KotlinVersion.KOTLIN_1_9)
-                apiVersion.set(KotlinVersion.KOTLIN_1_9)
-            }
-        }
-    }
 }
 
 tasks.register<Delete>("clean") {
